f8e5e3ffbbb3f5290d32bc67de9dfcc0f25d37d1
WUYUDING <e0533355@u.nus.edu>
2020-05-15 16:30:58 +0800

    'initial...'

--- pages/index/index.js
+++ pages/index/index.js
@@ -58,15 +58,39 @@ Page({
     }],
   },
   onLoad:function(){
-    const thiz=this;
-    wx.getUserInfo({
-      success:function(res){
-        app.globalData.userInfo = res.userInfo;
-        thiz.setData({
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
           userInfo: res.userInfo,
-          hasUserInfo: true
+          hasUserInfo: true,
         })
       }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
     })
   },
   showModal(e) {
@@ -85,5 +109,5 @@ Page({
       TabCur: e.currentTarget.dataset.id,
       scrollLeft: (e.currentTarget.dataset.id - 1) * 60
     })
-  }
+  },
 })
