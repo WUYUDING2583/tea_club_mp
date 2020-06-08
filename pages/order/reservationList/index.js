6f799a5822c68570b94a4e8daefdd9ea4ee97d01
WUYUDING <e0533355@u.nus.edu>
2020-06-08 13:38:46 +0800

    '我的预约界面完成'

--- pages/order/reservationList/index.js
+++ pages/order/reservationList/index.js
@@ -316,6 +316,13 @@ Page({
       })
   },
 
+  showShopAddress:function(e){
+    this.setData({
+      modalName:"shopAddressModal",
+      shopAddress:e.currentTarget.dataset.target
+    })
+  },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
